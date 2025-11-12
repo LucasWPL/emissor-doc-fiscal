b6c461cf4aec1a16f719e0c81160ce1e25dd3c14
Pedro Lucas <pedro.lucaswpl@gmail.com>
2025-11-11 21:18:30 -0300

    feat: get cadastro

---
+++ src/Services/CommonService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace LucasWPL\EmissorDocFiscal\Services;
+
+use NFePHP\Common\Certificate;
+use NFePHP\NFe\Common\Standardize;
+use NFePHP\NFe\Tools;
+
+class CommonService
+{
+    public string $config;
+
+    private Tools $tools;
+
+    private Certificate $certificate;
+
+    public function __construct(string $configFilePath, string $certificateFilePath, string $certificatePassword)
+    {
+        $this->config = file_get_contents($configFilePath);
+        $this->certificate = Certificate::readPfx(file_get_contents($certificateFilePath), $certificatePassword);
+
+        $this->tools = new Tools($this->config, $this->certificate);
+    }
+
+
+    public function getCadastro(string $UF, string $CNPJ): object 
+    {
+        return (new Standardize($this->tools->sefazCadastro($UF, $CNPJ)))->toStd();
+    }
+}
